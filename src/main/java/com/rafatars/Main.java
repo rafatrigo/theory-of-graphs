419c809609d6ce5ffd7dd7ae7e750e0d6560d3bd
rafatars <rafael.trigo.azevedo@gmail.com>
2024-03-29 16:55:59 -0300

    Remove unnecessary code from the main

--- src/main/java/com/rafatars/Main.java
+++ src/main/java/com/rafatars/Main.java
@@ -1,6 +1,5 @@
 package com.rafatars;
 
-import com.rafatars.classes.Analyzer;
 import com.rafatars.classes.Graph;
 
 public class Main {
@@ -8,24 +7,9 @@ public class Main {
         
         String graphPath = "src/main/resources/graph.txt";
 
-        Graph graph = new Graph(graphPath);
+        Graph graph = new Graph(graphPath);        
         
-        Analyzer analyzer = new Analyzer(graph);
-
-        System.out.println("O grafo é simples: " + !analyzer.isDirected(graph));
-        System.out.println("Ordem do grafo: " + analyzer.order(graph));
-        System.out.println("O tamanho do grafo é: " + analyzer.size(graph));
-        System.out.println("É conexo: " + analyzer.isConnected(graph));
-        System.out.println("É um pseudografo: " + analyzer.isPseudograph(graph));
-        System.out.println("É reflexivo: " + analyzer.isReflective(graph));
-        System.out.println("É ponderado: " + analyzer.isWeighted(graph));
-        System.out.println("É multigrafo: " + analyzer.isMultigraph(graph));
-        System.out.println("É vazio: " + analyzer.isEmpty(graph));
-        System.out.println("É trivial: " + analyzer.isTrivial(graph));
-
-
-
-
-        
+        graph.analyze();
+        graph.showAnalyze();
     }
 }
