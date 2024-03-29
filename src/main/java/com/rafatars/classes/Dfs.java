0d032473b499bfb11a00dd7b45c6c48546604916
rafatars <rafael.trigo.azevedo@gmail.com>
2024-03-29 16:15:05 -0300

    Create the Deapth First Search Algorithm

---
+++ src/main/java/com/rafatars/classes/Dfs.java
@@ -0,0 +1,36 @@
+package com.rafatars.classes;
+
+import java.util.ArrayList;
+
+import lombok.Data;
+
+//Depth First Search Algorithm
+@Data
+public class Dfs {
+    private Graph graph;
+    private boolean visited[];
+
+    public Dfs(Graph graph) {
+        this.graph = graph;
+
+        //initialize the visited array with the size of the number of vertices
+        //and set all values to false
+        this.visited = new boolean[graph.getVertices().size()];
+    }
+
+    public void dfs(Vertex vertex) {
+        
+        if(visited[vertex.getNumber()]) {
+            return;
+        }
+
+        visited[vertex.getNumber()] = true;
+
+        Vertex[] neighbours = vertex.getNeighbours();
+
+        for(Vertex neighbour : neighbours) {
+            dfs(neighbour);
+        }
+    }
+
+}
