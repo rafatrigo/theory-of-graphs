1c11430342104c79977caaed6b439e49b6d8bf28
rafatars <rafael.trigo.azevedo@gmail.com>
2024-03-29 20:34:29 -0300

    Add the isComplete function

--- src/main/java/com/rafatars/classes/Analyzer.java
+++ src/main/java/com/rafatars/classes/Analyzer.java
@@ -1,5 +1,7 @@
 package com.rafatars.classes;
 
+import java.util.ArrayList;
+
 import lombok.Data;
 
 @Data
@@ -20,6 +22,36 @@ public class Analyzer {
             if (!visited) {
                 return false;
             }
+        }
+
+        return true;
+    }
+
+    //verify if each vertex has a edge to all other vertices
+    public boolean isComplete(Graph graph) {
+        for (Vertex vertex : graph.getVertices()) {
+            
+            //create a list of visited vertices
+            //initialize all values to false
+            boolean[] visited = new boolean[graph.getVertices().size()];
+
+            for (Vertex neighbour : vertex.getNeighbours()) {
+                
+                visited[neighbour.getNumber()] = true;
+                
+            }
+
+            //mark itself as visited
+            visited[vertex.getNumber()] = true;
+
+            //if contains a false value, it is not complete
+            //because it means that a vertex dont have a edge to all other vertices
+            for (boolean visit : visited) {
+                if (!visit) {
+                    return false;
+                }
+            }
+
         }
 
         return true;
