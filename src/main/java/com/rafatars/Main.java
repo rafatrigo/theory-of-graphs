31fd29b2ca54816328f20a4643bdd96f94da437f
rafatars <rafael.trigo.azevedo@gmail.com>
2024-03-29 16:23:13 -0300

    Add functions to test the analyzer and change the graph.txt

--- src/main/java/com/rafatars/Main.java
+++ src/main/java/com/rafatars/Main.java
@@ -12,8 +12,20 @@ public class Main {
         
         Analyzer analyzer = new Analyzer(graph);
 
-        System.out.println("O grafo é simples: " + analyzer.isDirected(graph));
+        System.out.println("O grafo é simples: " + !analyzer.isDirected(graph));
+        System.out.println("Ordem do grafo: " + analyzer.order(graph));
         System.out.println("O tamanho do grafo é: " + analyzer.size(graph));
+        System.out.println("É conexo: " + analyzer.isConnected(graph));
+        System.out.println("É um pseudografo: " + analyzer.isPseudograph(graph));
+        System.out.println("É reflexivo: " + analyzer.isReflective(graph));
+        System.out.println("É ponderado: " + analyzer.isWeighted(graph));
+        System.out.println("É multigrafo: " + analyzer.isMultigraph(graph));
+        System.out.println("É vazio: " + analyzer.isEmpty(graph));
+        System.out.println("É trivial: " + analyzer.isTrivial(graph));
+
+
+
+
         
     }
 }
